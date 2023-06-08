59bea2347ed12dae03f96f1ade146adbcaad7a4f
Mikeg1804 <121838324+Mikeg1804@users.noreply.github.com>
2023-06-07 20:09:10 -0700

    trying to get the all records to show

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,20 +1,47 @@
 const {User} = require('../model');
 
 const createUser = async (req, res) => {
-    const{userName, email, password, firstName, lastName} = req.body;
+    const{ userName, email, password, firstName, lastName} = req.body;
 
     try {
-        const newUser = await User.create({
+        const newUser = await User.create(
+            {
         userName,
         email,
         password,
         firstName,
         lastName,
-        });
+        }
+        // req.body
+        );
         res.json(newUser);
     } catch (error) {
         res.status(500).json(error);
     }
 };
 
-module.exports = {createUser};
+const getUser = async (req, res) => {
+    try {
+        const user = await User.findOne(
+            {
+        userName: req.params.userName,
+        }
+        ); 
+        res.json(user);    
+    } catch (error) {
+        res.status(500).json(error);  
+    }
+};
+
+const getAllUsers = async (req, res) => {
+    try {
+        const users = await User.findAll();
+        res.json(users);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+};
+
+
+module.exports = {createUser, getUser, getAllUsers};
+
--- routes/apiRoutes/userRoutes/index.js
+++ routes/apiRoutes/userRoutes/index.js
@@ -1,8 +1,10 @@
-const {createUser} = require('../../../controllers/userController');
+const {createUser, getUser, getAllUsers} = require('../../../controllers/userController');
 
 const router = require('express').Router();
 
 
 router.post('/', createUser);
+router.get('/singleuser', getUser);
+router.get('/users', getAllUsers);
 
 module.exports = router;
