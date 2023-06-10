25c45a7229e42b8051259243d3ba5fac5e3bf034
Mikeg1804 <121838324+Mikeg1804@users.noreply.github.com>
2023-06-10 13:46:15 -0700

    workingongettingsingleuser

--- routes/apiRoutes/userRoutes/index.js
+++ routes/apiRoutes/userRoutes/index.js
@@ -6,7 +6,7 @@ const router = require('express').Router();
 router.post('/', createUser);
 
 // the userName parameter added to this route is the same one withing the getUser function
-router.get('/:userName', getUser);
+router.get('/:_id', getUser);
 router.get('/', getAllUsers);
 router.post('/follower/:friend', addFriend);
 router.put('/', deleteUser);
